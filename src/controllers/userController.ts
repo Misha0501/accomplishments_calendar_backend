4ece002fd0fdf4e622e83f44eab890cedb3a3874
misha0501 <misha.galenda@gmail.com>
2024-05-21 13:11:14 +0400

    added name handling of a calendar

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,36 +1,36 @@
-import { Request, Response } from 'express';
-import Calendar from '../models/Calendar';
-import Day from '../models/Day';
+import { Request, Response } from 'express'
+import Calendar from '../models/Calendar'
+import Day from '../models/Day'
 
 export const registerUser = async (req: Request, res: Response) => {
-    const { calendarData } = req.body;
-    const user = req.user;
+    const { calendarData, calendarName } = req.body
+    const user = req.user
 
     if (!user) {
-        return res.status(401).json({ message: 'No authenticated user' });
+        return res.status(401).json({ message: 'No authenticated user' })
     }
 
     // @ts-ignore
-    const userId = user.uid;
+    const userId = user.uid
 
     try {
         const newCalendar = new Calendar({
-            name: "My Calendar",
+            name: calendarName,
             user: userId
-        });
-        const savedCalendar = await newCalendar.save();
+        })
+        const savedCalendar = await newCalendar.save()
 
         const days = calendarData.map(day => ({
             date: day.date,
             day: day.day,
             isActive: day.isActive,
             calendar: savedCalendar._id
-        }));
-        await Day.insertMany(days);
+        }))
+        await Day.insertMany(days)
 
-        res.status(201).json({ message: 'User registered and calendar transferred successfully' });
+        res.status(201).json({ message: 'User registered and calendar transferred successfully', calendarId: savedCalendar._id })
     } catch (error) {
-        console.error('Failed to register user:', error);
-        res.status(500).json({ message: 'Failed to register user', error: error.message });
+        console.error('Failed to register user:', error)
+        res.status(500).json({ message: 'Failed to register user', error: error.message })
     }
-};
+}
